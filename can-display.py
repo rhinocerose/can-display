df68dbe0d50720795cace7c4d0908128ae13e574
rhinocerose <39277896+rhinocerose@users.noreply.github.com>
2021-06-17 13:32:25 -0400

    Update can-display.py

--- can-display.py
+++ can-display.py
@@ -1,17 +1,33 @@
 import can
 import binascii
 
+from rich import box
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.style import Style
+from rich.table import Table
+from rich.live import Live
+from rich.text import Text
+
 class CanDisplay:
     "This is a class to display parsed CAN bus data in a TUI"
 
     CELL_VOLTAGE_FRAMES = ['1811f580', '1811f581', '1811f582', '1811f583']
-    ERROR_FRAMES = ['1811f580', '1811f581', '1811f582', '1811f583']
+    ERROR_FRAMES = ['183fcce8']
 
     can0 = can.interface.Bus(channel='can0', bustype='socketcan_native')
 
     def __init__(self):
         self.SLAVE_CELLS = [[None for i in range(48)], [None for i in range(32)], [None for i in range(28)], [None for i in range(48)]]
+        self.layout = Layout()
+        
+    def add_color(self, string, color):
+        text = "[" + color + "]" + str(string) + "[/" + color + "]"
+        return text
 
+    def new_line(self, number):
+        return "\n" * number
+    
     def make_cell_voltage_array(self, data, slave_num):
         index = int(data[:2], 16) - 1
         print("Slave Number: " + str(slave_num))
@@ -22,6 +38,10 @@ class CanDisplay:
             last = first + 4
             self.SLAVE_CELLS[int(slave_num)][index + i] = data[first:last]
 
+    def make_cell_table(self):
+        while self.SLAVE_CELLS[1][-1] == None:
+            self.SLAVE_CELLS[1].pop()
+        
     def read_can_messages(self):
         msg = self.can0.recv(10.0)
         if msg:
